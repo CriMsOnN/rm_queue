7944680a803d6487d5ce7788bc5a9b697a5dec15
Tasos <tasosmoustakis1995@gmail.com>
2022-09-20 19:33:40 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,9 +2,8 @@ fx_version 'cerulean'
 game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'RMCore is a roleplay framework for redm. Vite boilerplate used is from overextended'
 
-name 'rm_core'
+name 'rm_queue'
 version '0.0.1'
 license 'MIT'
 author 'Cr1MsOn'
